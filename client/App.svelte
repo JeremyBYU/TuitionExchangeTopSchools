2604dd0d7a0f884c3bf62dc68b470d6447b107fc
Jeremy Castagno <jeremybyu@gmail.com>
2022-03-27 18:09:49 -0400

    Updated Website

--- client/App.svelte
+++ client/App.svelte
@@ -1,89 +1,73 @@
 <script>
   import data from '../data/combined_data.json';
-  import { Datatable } from 'svelte-simple-datatables';
-  const settings = { columnFilter: true, css: true };
-  let rows;
+  import { html } from "gridjs";
+  import Grid from "gridjs-svelte"
+
+  const settings = {
+	  columns: [
+		  {name: "Name", id: "displayName"},
+		  {name: "State", id: "state"},
+		  {name: "Category", id: "rankingDisplayName"},
+		  {name: "Rank", data: (row) => row.rankingSortRank < 0? 1000: row.rankingSortRank},
+		  {name: "Tuition Exch.", id:"urlTE", formatter: (row) => html(`<a href='${row}' target='__blank__'>Link</a>`)},
+		  {name: "US News", id:"urlUSNews", formatter: (row) => html(`<a href='${row}' target='__blank__'>Link</a>`)},
+		],
+		pagination: {
+			limit: 15
+		},
+		sort: true,
+		fixedHeader: true,
+		search: true,
+  }
 </script>
 
-<main>
-<div id="main-container">
-	<!-- <div id="container-1"></div> -->
-	<div id="container-2">
-		<Datatable {settings} {data} bind:dataRows={rows}>
-		  <thead>
-			<th data-key="displayName">First Name</th>
-			<th data-key="state">State</th>
-			<th data-key="rankingDisplayName">Ranking Category</th>
-			<th data-key="rankingDisplayRank">Rank</th>
-			<th >Tution Exch.</th>
-			<th >US News</th>
-		  </thead>
-		  <tbody>
-			{#if rows}
-			  {#each $rows as row}
-				<tr>
-				  <td>{row.displayName}</td>
-				  <td>{row.state}</td>
-				  <td>{row.rankingDisplayName}</td>
-				  <td>{row.rankingDisplayRank}</td>
-				  <td><a href="{row.urlTE}">Link</a></td>
-				  <td><a href="{row.urlUSNews}">Link</a></td>
-				</tr>
-			  {/each}
-			{/if}
-		  </tbody>
-		</Datatable>
+<article>
+  <div class="wrapper">
+    <div class="one" style="">
+		<div style="height:60;justify-content: center; ">
+			<h1 style="text-align:center;">Tuition Exchange Member School Ranking</h1>
+		</div>
 	</div>
-	<!-- <div id="container-3"> -->
+    <div class="three" style="height:800px">
+		<Grid {data} {...settings} />
+    </div>
+  </div>
+</article>
 
-</div>
-</main>
-
-<style>
+<style global>
+	@import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
+  article {
+    max-width: 1800;
+    padding-bottom: 80px;
+	margin: 0 auto !important; 
+  }
+  .wrapper {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 10px;
+    grid-auto-rows: minmax(80, auto);
+    background: rgba(255, 62, 0, 0.1);
+    padding: 8px;
+  }
+  div {
+    border-radius: 8px;
+  }
+  .one {
+    background: rgba(255, 62, 0, 0.2);
+    grid-column: 1 / 4;
+    grid-row: 1;
+  }
+  .three {
+    background: #ffffff;
+    padding: 8px 0;
+    grid-column: 1/5;
+    grid-row: 2 / 4;
+  }
   td {
     text-align: left;
-    padding: 4px 0;
+    padding: 4px;
   }
-  
-body, div{
-	margin: 0;
-	padding: 0;
-	border: 0;
-	height: 100vh; 
-  	box-sizing: border-box;
-}
 
-/*You can use flex for this excercise*/
-#main-container{
-  height: 100%; 
-  border: 1px solid gray;
-  display: -webkit-flex;
-  display: flex; 
-  -webkit-flex-direction: column;
-   flex-direction: column; /**make the direction vertival*/
-   box-sizing: border-box;/*include the padding and border included in the elements total sizet*/
-   
-}
-
-#container-1{
-  background-color: red;
-  border: 1px solid gray;
-  height: 20px;
-  box-sizing: border-box;/*include the padding and border included in the elements total sizet*/
-  
-}
-
-#container-2{
-  background-color: blue;
-  border: 1px solid gray;
-  flex-grow: 1; /**Enable the growth of this element*/
-  box-sizing: border-box;/*include the padding and border included in the elements total sizet*/
-}
-
-#container-3{
-  background-color: green;
-  border: 1px solid gray;
-  height: 20px;
-  box-sizing: border-box;/*include the padding and border included in the elements total sizet*/
-}
 </style>
+
+
