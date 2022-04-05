571268b48fa6fc0fa488ffc04b47f81e3447fc3c
Jeremy Castagno <jeremybyu@gmail.com>
2022-04-04 21:10:14 -0400

    Fix some styling issues

--- client/App.svelte
+++ client/App.svelte
@@ -14,7 +14,7 @@
       data: data_mapped,
       columns: [
         { data: 'displayName', title: 'Name' },
-        { data: 'state', title: 'State' },
+        { data: 'state', title: 'State', width: 70, },
         { data: 'rankingDisplayName', title: 'Category' },
         { data: 'rankingSortRank', title: 'Rank' },
         {
@@ -23,6 +23,7 @@
           render: (data, type, row) => {
             return `<a href='${data}' target='__blank__'>Link</a>`;
           },
+          orderable: false 
         },
         {
           data: 'urlUSNews',
@@ -30,6 +31,7 @@
           render: (data, type, row) => {
             return `<a href='${data}' target='__blank__'>Link</a>`;
           },
+          orderable: false 
         },
       ],
       buttons: ['copy', 'excel', 'pdf'],
@@ -40,7 +42,7 @@
             var column = this;
             window.mytest = column;
             console.log('test');
-            var select = jq('<select><option value=""></option></select>')
+            var select = jq('<select class="form-select form-select-sm"><option value=""></option></select>')
               .appendTo(
                 jq('#mytable thead tr:eq(1) th').eq(column.index()).empty(),
               )
@@ -66,7 +68,7 @@
             console.log('that');
             let title = "";
             var select = jq(
-              '<input type="text" placeholder="Search ' + title + '" />',
+              '<input class="form-control form-control-sm" type="text" placeholder="Search ' + title + '" />',
             )
               .appendTo(
                 jq('#mytable thead tr:eq(1) th').eq(column.index()).empty(),
@@ -105,13 +107,14 @@
 
       <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
         <a class="me-3 py-2 text-dark text-decoration-none" href="#">Data</a>
-        <a class="me-3 py-2 text-dark text-decoration-none" href="#">About</a>
+        <a class="me-3 py-2 text-dark text-decoration-none" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">About</a>
       </nav>
     </div>
 
     <div class="pricing-header p-1 pb-md-4 mx-auto text-center">
       <h3 class="display-6 fw-normal">Data</h3>
-      <p class="fs-5 text-muted">See all the top schools</p>
+      <p class="fs-5 text-muted">See all the top schools under the <a href="https://www.tuitionexchange.org/" target='__blank__'>Tution Exchange</a> program according to <a href="https://www.usnews.com/best-colleges" target='__blank__'>US News</a>. 
+        You can filter the data by state, category, and school name. You can also sort by clicking the column headers at the top. </p>
     </div>
   </header>
 
@@ -141,5 +144,28 @@
   </main>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">About Site</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Hi! I'm <a href="https://robosim.dev/pages/about/" target='__blank__'>Jeremy</a>, a software engineer and Professor. <br><br>
+        I made this website to see how the schools in the <a href="https://www.tuitionexchange.org/" target='__blank__'>Tution Exchange</a>  program are ranked according to <a href="https://www.usnews.com/best-colleges" target='__blank__'>US News</a>. 
+        I have three children and wanted to find the top ranked colleges with free/reduced tuition. I hope this may be useful for you! <br><br>
+        If you want to see how I gathered the data and made the website, check out the  <a href="https://github.com/JeremyBYU/TuitionExchangeTopSchools" target='__blank__'>Github Repository</a> 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <style global>
+  select { min-width: 100; }
 </style>
